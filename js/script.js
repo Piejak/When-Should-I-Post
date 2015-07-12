4af3656d2828a37694fb7c27c151cd2051eeba72
Brian Piejak <Brian.Piejak@gmail.com>
2015-07-12 01:35:41 -0400

    App now gets likes and time
    The app gets all of the user's pictures posted times and the likes that
    those pictures received. The time is also displayed in a readable
    manner, rather than as a unix timestamp. Now I just need to make the
    values graph and do some styling and I'll be able to wrap this up.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,41 @@
 $( function() {
+  var postTime = [];
+  var likes = [];
   var parser = document.createElement('a');
-  parser.href = window.location.href;
+  // parser.href = window.location.href;
+  parser.href = "http://piejak.github.io/When-Should-I-Post/success.html#access_token=23817452.550939d.65b883ca68a543adaba9d68a95846c96"
 
   var rawAccessToken = parser.hash;
-  var accessToken = rawAccessToken.substring("#access_token".length);
-  console.log(accessToken);
+  var accessToken = rawAccessToken.substring("#access_token=".length);
+  var feedURL = "https://api.instagram.com/v1/users/self/media/recent/?access_token=";
+
+  $.ajax({
+    url: feedURL + accessToken,
+
+    jsonp: "callback",
+
+    dataType: "jsonp",
+
+    data: {
+      q: "created_time",
+      format: "json"
+    },
+
+    success: function(response) {
+      console.log(response);
+      $.each(response.data, function(index) {
+        var mill = response.data[index].created_time * 1000;
+        var d = new Date(mill)
+        var hour = d.getHours();
+        var minute = d.getMinutes();
+        postTime.push(hour + ":" + minute);
+        likes.push(response.data[index].likes.count);
+      });
+      console.log(postTime);
+      console.log(likes);
+    }
+  });
+
+
+  return false;
 });
