95249f0e166cbb1ba8f841d2aad38e9c8d9a2e39
Brian Piejak <Brian.Piejak@gmail.com>
2015-07-11 23:29:45 -0400

    I didn't want an alert, I just wanted to get the variable, I'm dumb

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $( function() {
   var parser = document.createElement('a');
   parser.href = window.location.href;
 
-  var accessToken = parser.hash;
-  alert(accessToken.substring('#access_token='.length));
+  var rawAccessToken = parser.hash;
+  var accessToken = rawAccessToken.substring('#access_token='.length));
   console.log(accessToken);
 });
