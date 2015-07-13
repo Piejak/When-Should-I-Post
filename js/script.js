4e1d59f09ec4ce69f75d022b06578fdeb605e277
Brian Piejak <Brian.Piejak@gmail.com>
2015-07-12 23:03:40 -0400

    sorts and displays correctly

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $( function() {
   var likes = [];
   var lapt = [];
   var sortTime =[];
+  var sortedPostTime = [];
+  var sortedLikes = [];
   var parser = document.createElement('a');
   //parser.href = window.location.href;
   parser.href = "http://piejak.github.io/When-Should-I-Post/success.html#access_token=23817452.550939d.65b883ca68a543adaba9d68a95846c96"
@@ -28,10 +30,11 @@ $( function() {
       $.each(response.data, function(index) {
         lapt.push([]);
         var mill = response.data[index].created_time * 1000;
-        sortTime.push(mill);
         var d = new Date(mill)
         var hour = d.getHours();
         var minute = d.getMinutes();
+        var totalMinutes = (hour * 60) + minute;
+        sortTime.push(totalMinutes);
         if(hour > 12) {
           postTime.push((hour - 12) + ":" + minute + " PM");
         } else if (hour === 12) {
@@ -46,19 +49,33 @@ $( function() {
         lapt[i].push(postTime[i]);
         lapt[i].push(likes[i]);
       });
+      lapt.sort(function(a,b) {
+        if (a[0] > b[0]) {
+          return 1;
+        }
+        if (a[0] < b[0]) {
+          return -1;
+        }
+        else {
+          return 0;
+        }
+      });
+      $.each(lapt, function(j) {
+        sortedPostTime.push(lapt[j][1]);
+        sortedLikes.push(lapt[j][2]);
+      })
 
-      console.log(postTime);
-      console.log(likes);
-      console.log(lapt);
+      console.log(sortedPostTime);
+      console.log(sortedLikes);
 
       new Chartist.Line('.ct-chart', {
-        labels: postTime,
+        labels: sortedPostTime,
         series: [
-          likes
+          sortedLikes
         ]
       }, {
-        high: 115,
-        low: 10,
+        high: Math.max.apply(null, likes),
+        low: Math.min.apply(null, likes),
         fullWidth: true,
         // As this is axis specific we need to tell Chartist to use whole numbers only on the concerned axis
         axisY: {
